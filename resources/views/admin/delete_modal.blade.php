cee72d1c71775a102bd77c85a8fa8ae8499853bf
Ivan Ramirez <ivan_amigue@hotmail.com>
2021-09-08 22:27:58 -0500

    Corrección icono cerrar modal

--- resources/views/admin/delete_modal.blade.php
+++ resources/views/admin/delete_modal.blade.php
@@ -3,9 +3,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title" id="user_delete_confirm_title">{{ trans('strings.crud.delete') }} {{ substr($word, 0, -1) }}</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         {{ trans('strings.crud.delete_question') }}
--- resources/views/admin/restore_modal.blade.php
+++ resources/views/admin/restore_modal.blade.php
@@ -3,9 +3,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title" id="user_restore_confirm_title">{{ trans('strings.crud.restore') }} {{ substr($word, 0, -1) }}</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         {{ trans('strings.crud.restore_question') }}
